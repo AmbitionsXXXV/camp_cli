4463fa2848d9df0b1ee775caf2b2b4c50a307abc
smallbin <3057988675@qq.com>
2023-07-26 19:57:11 +0800

    添加commander

--- bin/index.ts
+++ bin/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+/* eslint-disable */
 console.log('hello world')
 const program = require('commander')
 program.version(require('../package').version).usage('<command> [options]')
