6386de45eec3fdf75952586a0f548aec4197f541
etctera <yilonglei790@gmail.com>
2023-07-27 14:26:10 +0800

    chore: 工具函数分块

--- bin/index.ts
+++ bin/index.ts
@@ -1,51 +1,8 @@
 #!/usr/bin/env node
 
 import fs from "fs"
-import path from "path"
 import { program } from "commander"
-
-const analyzed = new Set<string>()
-let dependencyGraph: { [key: string]: any } = {}
-// This object will hold the dependency graph
-
-function getPackageJsonContent(filePath: string) {
-  const content = fs.readFileSync(filePath, "utf-8")
-  return JSON.parse(content)
-}
-
-function traverseDependencies(
-  packagePath: string,
-  options: any,
-  currentDepth: number = 0
-) {
-  const packageJsonPath = path.join(packagePath, "package.json")
-  if (!fs.existsSync(packageJsonPath)) {
-    return
-  }
-
-  const packageJsonContent = getPackageJsonContent(packageJsonPath)
-  const dependencies = packageJsonContent.dependencies || {}
-  const peerDependencies = packageJsonContent.peerDependencies || {}
-  const devDependencies = packageJsonContent.devDependencies || {}
-
-  const packageName = packageJsonContent.name
-  if (analyzed.has(packageName)) {
-    return
-  }
-
-  analyzed.add(packageName)
-  dependencyGraph[packageName] = dependencies
-
-  if (currentDepth < options.depth) {
-    for (const dep in { ...dependencies, ...peerDependencies, ...devDependencies }) {
-      traverseDependencies(
-        path.join(packagePath, "node_modules", dep),
-        options,
-        currentDepth + 1
-      )
-    }
-  }
-}
+import { dependencyGraph, traverseDependencies } from "../lib"
 
 program
   .command("analyze")
@@ -56,14 +13,19 @@ program
     options.depth = options.depth === "Infinity" ? Infinity : parseInt(options.depth)
     traverseDependencies(process.cwd(), options)
     const jsonString = JSON.stringify(dependencyGraph, null, 2)
-    // Write the dependency graph to a JSON file if --json is provided
-    fs.writeFile(options.json, jsonString, (err) => {
+
+    if (options.json) {
+      // Write the dependency graph to a JSON file if --json is provided
+      fs.writeFile(options.json, jsonString, err => {
         if (err) {
-          console.error("JSON file is not", err);
+          console.error("JSON file is not", err)
         } else {
-          console.log("JSON file created successfully.");
-        }})
-    
+          console.log("JSON file created successfully.")
+        }
+      })
+    } else {
+      // Otherwise, open a website to visualize the dependency graph
+    }
   })
 
 program.parse(process.argv)
---
+++ lib/index.ts
@@ -0,0 +1,45 @@
+import fs from "fs"
+import path from "path"
+
+const analyzed = new Set<string>()
+export const dependencyGraph: { [key: string]: any } = {}
+// This object will hold the dependency graph
+
+export function getPackageJsonContent(filePath: string) {
+  const content = fs.readFileSync(filePath, "utf-8")
+  return JSON.parse(content)
+}
+
+export function traverseDependencies(
+  packagePath: string,
+  options: any,
+  currentDepth: number = 0
+) {
+  const packageJsonPath = path.join(packagePath, "package.json")
+  if (!fs.existsSync(packageJsonPath)) {
+    return
+  }
+
+  const packageJsonContent = getPackageJsonContent(packageJsonPath)
+  const dependencies = packageJsonContent.dependencies || {}
+  const peerDependencies = packageJsonContent.peerDependencies || {}
+  const devDependencies = packageJsonContent.devDependencies || {}
+
+  const packageName = packageJsonContent.name
+  if (analyzed.has(packageName)) {
+    return
+  }
+
+  analyzed.add(packageName)
+  dependencyGraph[packageName] = dependencies
+
+  if (currentDepth < options.depth) {
+    for (const dep in { ...dependencies, ...peerDependencies, ...devDependencies }) {
+      traverseDependencies(
+        path.join(packagePath, "node_modules", dep),
+        options,
+        currentDepth + 1
+      )
+    }
+  }
+}
